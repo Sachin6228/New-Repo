ac5ae66765224c4706ae0204134c3739e95f1b96
Sachin Pathare <sachin.pathare427@gmail.com>
2022-08-30 13:32:50 +0530

    Sachin Commit and push

---
+++ NewRepo/src/package01/TestClass1.java
@@ -0,0 +1,11 @@
+package package01;
+
+public class TestClass1 
+{
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("TC1");
+	}
+}
---
+++ NewRepo/src/package01/TestClass2.java
@@ -0,0 +1,11 @@
+package package01;
+
+public class TestClass2 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("TC2");
+	}
+}
